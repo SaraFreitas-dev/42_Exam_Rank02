59e843cef152541f3d72b570a83b1a99b72b8107
Sara Fontes <sfontes94@gmail.com>
2025-12-07 19:53:11 +0000

    updated level_1

--- Level_1/rotone/rotone.c
+++ Level_1/rotone/rotone.c
@@ -10,3 +10,35 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+char	replace_c(char c)
+{
+	if (((c >= 'A') && (c <= 'Y')) || ((c >= 'a') && (c <= 'y')))
+		c += 1;
+	else if (c == 'Z')
+		c = 'A';
+	else if (c == 'z')
+		c = 'a';
+	return (c);
+}
+
+int	main(int argc, char *argv[])
+{
+	int	j;
+
+	if (argc != 2)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+	j = 0;
+	while (argv[1][j])
+	{
+		argv[1][j] = replace_c(argv[1][j]);
+		write(1, &argv[1][j], 1);
+		j++;
+	}
+	write(1, "\n", 1);
+	return (0);
+}
