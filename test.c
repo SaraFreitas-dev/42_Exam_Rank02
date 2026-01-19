87e1f4fe5aa0bcea4ea416af5387580cdee9b8f4
Sara Fontes <sfontes94@gmail.com>
2026-01-19 16:55:20 +0000

    added level_2 exercises

--- test.c
+++ test.c
@@ -9,3 +9,74 @@
 /*   Updated: 2026/01/10 11:08:23 by sarfreit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	nbr;
+	int	sign;
+
+	i = 0;
+	nbr = 0;
+	sign = 1;
+
+	while ((str[i] == 32) || ((str[i] >= 9) && (str[i] <= 13)))
+		i++;
+	if ((str[i] == '-') || (str[i] == '+'))
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while ((str[i] >= '0') && (str[i] <= '9'))
+	{
+		nbr = nbr * 10 + (str[i] - 48);
+		i++;
+	}
+	return (nbr * sign);
+}
+
+void ft_putnbr(int nbr)
+{
+	long 	n;
+	char	c;
+
+	n = (long)nbr;
+	if (n < 0)
+	{
+		write(1, "-", 1);
+		n = -n;
+	}
+	if (n > 9)
+		ft_putnbr(n / 10);
+	c = (n % 10) + 48;
+	write(1, &c , 1);
+}
+
+int	main(void)
+{
+	char *str = "-123";
+	int nbr = ft_atoi(str);
+	ft_putnbr(nbr);
+
+	return (0);
+}
+/*Write a program that takes a positive integer as argument and displays the sum
+of all prime numbers inferior or equal to it followed by a newline.
+
+If the number of arguments is not 1, or the argument is not a positive number,
+just display 0 followed by a newline.
+
+Yes, the examples are right.
+
+Examples:
+
+$>./add_prime_sum 5
+10
+$>./add_prime_sum 7 | cat -e
+17$
+$>./add_prime_sum | cat -e
+0$
+$>*/
