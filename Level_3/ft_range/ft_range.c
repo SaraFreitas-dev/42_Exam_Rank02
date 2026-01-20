5de080da5df5c7caa247663e3d5f583eb3023b86
Sara Raquel Freitas Fontes <sarfreit@c2r2s3.42porto.com>
2026-01-20 18:20:24 +0000

    added Level_3 exercises

---
+++ Level_3/ft_range/ft_range.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sarfreit <sarfreit@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/20 17:44:06 by sarfreit          #+#    #+#             */
+/*   Updated: 2026/01/20 18:20:00 by sarfreit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int     *ft_range(int start, int end)
+{
+    int *arr;
+    int i;
+    int nbr;
+
+    if (end > start)
+        arr = malloc(((end - start) + 1) * sizeof(int));
+    else if (end < start)
+        arr = malloc(((start - end) + 1) * sizeof(int));
+    else
+        arr = malloc(sizeof(int));
+    if (!arr)
+        return (0);
+    i = 0;
+    nbr = start;
+    if (end > start)
+        while (nbr <= end)
+            arr[i++] = nbr++;
+    else if (start > end)
+        while (nbr >= end)
+            arr[i++] = nbr--;
+    else
+        arr[i] = start;
+    return (arr);
+}
+
+/*
+#include <stdio.h>
+
+int main(void)
+{
+    int *nums = ft_range(0,-2);
+    int i = 0;
+    
+    while (i < 3)
+    {
+        printf("%d\n", nums[i]);
+        i++;
+    }
+    return (0);
+}*/
