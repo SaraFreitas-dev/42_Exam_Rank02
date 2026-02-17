68e123a2690bb9459b96814d2864653e48dd5c1f
Sara Freitas <105666041+SaraFreitas-dev@users.noreply.github.com>
2026-02-17 21:41:26 +0000

    Implement max function

--- Level_2/max/max.c
+++ Level_2/max/max.c
@@ -10,3 +10,31 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+int		max(int* tab, unsigned int len)
+{
+    unsigned int i;
+    int result;
+    
+    i = 0;
+    if (len == 0)
+        return (0);
+    result = tab[i];
+    while (i < len)
+    {
+        if (tab[i] > result)
+            result = tab[i];
+        i++;      
+    }
+    return (result);
+}
+
+/*
+#include <stdio.h>
+
+int main(void)
+{
+    int myNumbers[] = {25, 0, 1000, 50, 75, 100};
+    printf("%d", max(myNumbers, 3));
+    return (0);
+}
+*/
