a9e4a7be20c6d7509b3e16928eaf710514495287
Sara Fontes <sfontes94@gmail.com>
2026-03-13 21:35:13 +0000

    added files

---
+++ test.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sarfreit <sarfreit@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/12 14:00:23 by sarfreit          #+#    #+#             */
+/*   Updated: 2026/03/12 14:00:23 by sarfreit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+int is_space(char c)
+{
+    if ((c == 32) || ((c >= 9) && (c <= 13)))
+        return (1);
+    return (0);
+}
+    void ft_putnbr(int n)
+{
+    char c;
+
+    if (n >= 10)
+    {
+        ft_putnbr(n / 10);
+    }
+    if (n < 0)
+    {
+        n = -n;
+        write(1, "-", 1);
+    }
+    c = (n % 10) + '0';
+    write(1, &c, 1);
+}
+*/
+
