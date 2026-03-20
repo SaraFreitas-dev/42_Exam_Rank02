a3801fe4aa80f6fe96647eaaa4271ba9ef1e4cc2
Sara Fontes <sfontes94@gmail.com>
2026-03-20 20:57:48 +0000

    added files

--- Level_4/ft_list_remove_if/ft_list_remove_if.c
+++ Level_4/ft_list_remove_if/ft_list_remove_if.c
@@ -15,16 +15,22 @@
 void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 {
     t_list *curr;
-    t_list *prev;
+    t_list *temp;
 
-    curr = *begin_list;
-    prev = 0;
-    while (curr)
+    while (*begin_list && (*cmp)((*begin_list)->data, data_ref) == 0)
     {
-        if ((cmp)(curr->data, data_ref) == 0)
+        temp = *begin_list;
+        *begin_list = (*begin_list)->next;
+        free(temp);
+    }
+    curr = *begin_list;
+    while (curr && curr->next)
+    {
+        if ((*cmp)(curr->next->data, data_ref) == 0)
         {
             temp = curr->next;
-            free(curr);
+            curr->next = temp->next;
+            free(temp);
         }
         else
             curr = curr->next;
